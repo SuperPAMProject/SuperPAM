b1aa5a6491ce82665d0a9aeb59b93d170058ddfb
Michael Tanner <michaelmontytanner@gmail.com>
2019-11-07 03:09:47 -0600

    Adding in recently made files

---
+++ sounds.py
@@ -0,0 +1,13 @@
+sounds = {
+    "highlight_btn":"sounds/select_1.wav",
+    "select_btn":"sounds/select_3.wav",
+    "end_of_list": "sounds/eol_1.wav",
+    "up_carousel": "sounds/up_carousel_2.wav",
+    "down_carousel": "sounds/down_carousel_2.wav",
+    "enter_menu": 'sounds/enter_menu.wav',
+    "exit_menu": "sounds/exit_menu.wav"
+}
+
+
+def getSound(sound):
+    return sounds[sound]
